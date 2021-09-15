16e2c48407fb4e72eca590482a5c1fa705425d1a
Valentine <ifeanyivalentine82@gmail.com>
2021-09-15 15:39:41 +0100

    testimonial

---
+++ src/components/Home/Testimonial/HomeTestimonial.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './HomeTestimonial.scss';
+
+const HomeTestimonial = () => {
+    return (
+        <div className='testimonial'>
+            <div className='rows'>
+                <div className='boder'>
+                    <div className='imago'>
+                        <img src='assets/kidder.png' alt='testimonial'/>
+                    </div>
+                </div>
+                <div className='test'>
+                    <h3>Valentine</h3>
+                    <h4>Age: 5</h4>
+                    <p>Ladipisicing elit. Quas explicabo dignissimos quisquam voluptatibus rem eum, recusandae earum assumenda odit. Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomeTestimonial
+
