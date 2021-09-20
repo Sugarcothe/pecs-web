cecd4e8121a33f69f884463e6db46c5d4db5ddf5
Valentine <ifeanyivalentine82@gmail.com>
2021-09-20 10:36:53 +0100

    frontend

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.scss';
+import Home2 from './Home2/Home2';
 import HomeTestimonial from './Testimonial/HomeTestimonial';
 
 const Home = () => {
@@ -27,29 +28,7 @@ const Home = () => {
                 </div>
            </div>
         
-            <div className='sections1'>
-                <div className='rows'>
-                    <div className='row1'>
-                        <h1>OUR RECORDS SPEAK FOR US</h1>
-                    </div>
-                    <div className='row2'>
-                        <h2>1K+</h2>
-                        <h4>Number of our pupils</h4>
-                    </div>
-                    <div className='row3'>
-                        <h2>150+</h2>
-                        <h4>Average Number of Pupils Yearly</h4>
-                    </div>
-                    <div className='row4'>
-                        <h2>100+</h2>
-                        <h4>Average number of learning materials</h4>
-                    </div>
-                    <div className='row4'>
-                        <h2>23</h2>
-                        <h4>Teachers spread accross the world</h4>
-                    </div>
-                </div>
-            </div>
+            <Home2/>
             <HomeTestimonial/>
 
 
