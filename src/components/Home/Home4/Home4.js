3b7f3fff8c93eb0dfda7621d207b9a5e999b4eda
Valentine <ifeanyivalentine82@gmail.com>
2021-09-27 12:37:12 +0100

    another section added

---
+++ src/components/Home/Home4/Home4.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Home4 = () => {
+    return (
+        <div className='Home4'>
+            <div className='section'>
+                <div className='header'>
+                    <h3>How it works</h3>
+                </div>
+                <div className='row'>
+                    <div className='row-1'>
+                        <div className='col-1'>
+                            <i class="fab fa-cloudversify"></i>
+                            <p>Personalized live-tutoring experience delivered by our expert Tutors</p>
+                        </div>
+                        <div className='col-1'>
+                            <i class="far fa-play-circle"></i>
+                            <p>Personalized live-tutoring experience delivered by our expert Tutors</p>
+                        </div>
+                        <div className='col-1'>
+                        <i class="fas fa-trophy"></i>
+                            <p>Personalized live-tutoring experience delivered by our expert Tutors</p>
+                        </div>
+                        
+                    </div>
+                    <div className='row-2'>
+                        <img src='assets/star.png' alt='starkids'/>
+                    </div> 
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home4
