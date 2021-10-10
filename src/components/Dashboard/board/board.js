15bd5f825d969e78880000cf1ac0a3ee236f852d
Valentine <ifeanyivalentine82@gmail.com>
2021-10-10 08:24:46 +0100

    board section

---
+++ src/components/Dashboard/board/board.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './board.scss';
+
+const board = () => {
+    return (
+        <div className='board'>
+            <div className='row'>
+
+                <div className='col-1'>
+
+                </div>
+
+                <div className='col-1'>
+
+                </div>
+
+                <div className='col-1'>
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default board
